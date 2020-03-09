4953cba22d8e6d5e8276de56dff2e76b83b30e77
kalczur <47967925+kalczur@users.noreply.github.com>
2020-03-09 21:39:56 +0100

    Add files via upload

--- script/Bullet.cs
+++ script/Bullet.cs
@@ -5,11 +5,12 @@ public class Bullet : Area2D
 {
     private const int speed = 500;
     private Vector2 velocity = new Vector2();
-    public float x;
-    public float y;
+    public Vector2 lookVec = new Vector2();
+    private float x;
+    private float y;
+    public int dmg;
     public override void _Ready()
     {
-        var lookVec = GetGlobalMousePosition() - GlobalPosition;
         x = lookVec.Normalized().x * speed;
         y = lookVec.Normalized().y * speed;
     }
@@ -21,8 +22,25 @@ public class Bullet : Area2D
         velocity.y = (y * delta);
         Translate(velocity);
     }
+
     public void _on_VisibilityNotifier2D_screen_exited()
     {
+        QueueFree();
+    }
+
+    public void _on_Bullet_body_entered(System.Object body)
+    {
+        if (body is Enemy)
+        {
+            Enemy tmpEnemy = (Enemy)body;
+            tmpEnemy.hit(dmg);
+        }
+        else if (body is Player)
+        {
+            Player tmpEnemy = (Player)body;
+            tmpEnemy.hit(dmg);
+        }
+
         QueueFree();
     }
 }
--- script/Enemy.cs
+++ script/Enemy.cs
@@ -1,25 +1,49 @@
 using Godot;
 using System;
-
 public class Enemy : KinematicBody2D
 {
     private const int speed = 100;
     private const int gravity = 50;
     private Vector2 velocity = new Vector2();
+    private Vector2 floor = new Vector2(0, -1);
+    public int hp = 100;
+    private PackedScene gun_1 = (PackedScene)GD.Load("res://scene/Gun_1.tscn");
+    private Gun gunNode = new Gun();
     public override void _Ready()
     {
-
+        gunNode = (Gun)gun_1.Instance();
+        AddChild(gunNode);
     }
     public override void _PhysicsProcess(float delta)
     {
+        if (hp <= 0)
+        {
+            kill();
+        }
+
         var vecToPlayer = GetTree().Root.GetNode<KinematicBody2D>("StageOne/Player").GlobalPosition - GlobalPosition;
 
-        if (Math.Abs(vecToPlayer.x) > 100)
+        if (Math.Abs(vecToPlayer.x) > 200)
             velocity.x = vecToPlayer.x > 0 ? speed : -speed;
         else
             velocity.x = 0;
 
         velocity.y += gravity;
-        MoveAndSlide(velocity);
+        MoveAndSlide(velocity, floor);
+        if (IsOnWall())
+            velocity.y = -600;
+    }
+    private void _on_Timer_timeout()
+    {
+        gunNode.shot();
+    }
+    public void hit(int dmg)
+    {
+        hp -= dmg;
+        velocity.y = -300;
+    }
+    public void kill()
+    {
+        QueueFree();
     }
 }
