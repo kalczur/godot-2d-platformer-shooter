b366d54378b6ffe75d6e593856adff62d442a6a1
kalczur <kalczur@gmail.com>
2020-03-20 19:14:13 +0100

    add c4

--- script/Gameplay.cs
+++ script/Gameplay.cs
@@ -5,6 +5,7 @@ public class Gameplay : Node2D
 {
   PackedScene enemy = GD.Load("res://scene/Enemy.tscn") as PackedScene;
   PackedScene firstAidKit = GD.Load("res://scene/FirstAidKit.tscn") as PackedScene;
+  PackedScene c4 = GD.Load("res://scene/C4.tscn") as PackedScene;
 
   private void SpawnEnemy()
   {
@@ -18,6 +19,12 @@ public class Gameplay : Node2D
     firstAidKitNode.Position = new Vector2((float)GD.RandRange(40, (GetViewportRect().Size.x) - 40), -100);
     AddChild(firstAidKitNode);
   }
+  private void SpawnC4()
+  {
+    var c4Node = c4.Instance() as FirstAidKit;
+    c4Node.Position = new Vector2((float)GD.RandRange(40, (GetViewportRect().Size.x) - 40), -100);
+    AddChild(c4Node);
+  }
   public void _on_EnemySpawnTimer_timeout()
   {
     SpawnEnemy();
@@ -25,5 +32,9 @@ public class Gameplay : Node2D
   public void _on_FirstAidKitSpawnTimer_timeout()
   {
     SpawnFirstAidKit();
+  }
+  public void _on_C4pawnTimer_timeout()
+  {
+    SpawnC4();
   }
 }
