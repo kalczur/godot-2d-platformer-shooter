9af471c6337e6865880b6e3332b750d1ace3f8d8
kalczur <kalczur@gmail.com>
2020-03-29 18:08:06 +0200

    fix

--- script/Options.cs
+++ script/Options.cs
@@ -3,8 +3,17 @@ using System;
 
 public class Options : Node2D
 {
+
+  public override void _Ready()
+  {
+
+  }
   public void _on_Back_pressed()
   {
     GetParent().GetNode<Camera2D>("TitleScreenCamera").Offset = new Vector2(0, 0);
+  }
+  public void _on_HSlider_changed()
+  {
+
   }
 }
