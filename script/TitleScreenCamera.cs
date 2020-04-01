dfcff55e27ec12106686326db0ae07fd6342f260
kalczur <kalczur@gmail.com>
2020-04-01 17:10:19 +0200

    Version 05.2

--- script/TitleScreenCamera.cs
+++ script/TitleScreenCamera.cs
@@ -3,4 +3,22 @@ using System;
 
 public class TitleScreenCamera : Camera2D
 {
+  bool where = true;
+
+  public override void _Process(float delta)
+  {
+    float tempZoom = Zoom.x;
+    if (where)
+    {
+      Zoom = new Vector2(tempZoom - 0.0001f, tempZoom - 0.0001f);
+      if (tempZoom < 0.9)
+        where = false;
+    }
+    else
+    {
+      Zoom = new Vector2(tempZoom + 0.001f, tempZoom + 0.001f);
+      if (tempZoom >= 0.999)
+        where = true;
+    }
+  }
 }
